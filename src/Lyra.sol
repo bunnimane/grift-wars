7e2d0c6f805e0cc2ed6b70dddb4b7cabe29ea967
Matt <guzzo.m@hotmail.com>
2022-10-24 14:36:06 -0400

    start lyra contract

---
+++ src/Lyra.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "../lib/ERC721A/contracts/ERC721A.sol";
+
+contract Lyra is ERC721A {
+    constructor() ERC721A("LYRA", "LYRA") {}
+
+    function mint(uint256 quantity) external payable {
+        // `_mint`'s second argument now takes in a `quantity`, not a `tokenId`.
+        _mint(msg.sender, quantity);
+    }
+}
