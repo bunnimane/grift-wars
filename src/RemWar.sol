78bd77ba0fc807754e28a06370c81b28b1912142
Matt <guzzo.m@hotmail.com>
2023-01-31 23:10:02 -0500

    clean up shoot function, add whitepaper2.txt

--- src/RemWar.sol
+++ src/RemWar.sol
@@ -28,97 +28,6 @@ error WarStarted();
 error WarOver();
 
 contract RemWar is Ownable, ReentrancyGuard {
-    /*
-    /// -------------------------------------
-    /// 🔫 Grift Wars
-    /// -------------------------------------
-
-    Premise: 
-    
-    - Each Remilio64 starts as ALIVE.
-
-    - Each Remilio64 starts with a bounty of 0.
-
-    - Each ALIVE Remilio64 can shoot someone with ether.
-
-    - MIN bullet cost is 0.0005Ξ FOR SANITY.
-
-    - NO friendly fire.
-
-    - IF an ALIVE Remilio64 (A) is shot by an ALIVE REMILIO (B):
-        - IF A's bounty is 0, it is DEAD. B's bounty increases
-        to X where X is the amount of ETHER B shot A with.
-
-        - IF A's bounty is not 0, A's bounty is subtracted by
-        X * (X/A's bounty) where X is the the amount of ETHER B shot A with and
-        added to B's bounty.
-            SO:
-            - B's bounty increases to X * (X/A's bounty).
-            - IF A's new bounty is 0, it is DEAD.
-            - IF A's new bounty is >0, it is still ALIVE with
-            with old_bounty - (X * (X/A's bounty)).
-
-            NOTE: The reason behind the shot not just subtracting,
-            but instead subtracting a percentage is because 'whales' would 
-            be to susceptible to getting ganged up on and there is 
-            NO INCENTIVE for them. This makes it harder to take 
-            down 'whales', but still provides value to higher
-            valued shots. This seems to be most fair to me.
-
-        - These bounty changes reflected in the FACTION_BOUNTY
-
-        EVERYTIME A REMILIO KILLS ANOTHER REMILIO, IT'S KILL
-        COUNT WILL INCREASE BY 1. THIS IS SIMPLY A VANITY METRIC
-        BUT A COOL FLEX 💪.
-
-    - There is a CUTOFF datetime when the war ends.
-
-    AFTER THE CUT OFF DATE:
-
-    - Each faction's TOTAL_BOUNTY is calculated.
-
-    - The winning faction will consume the balances of every
-    other faction's TOTAL_BOUNTY, leaving it with a FINAL_BOUNTY.
-
-    - The 33% of the FINAL_BOUNTY is transferred to the dev team. 
-
-    - 10% will go to EVERY REM64 on the winning faction who shot someone.
-
-    - Each token in the winning faction's bounty, will serve as a
-    claim on the (FINAL_BOUNTY * 0.57), proportional to (TOTAL_BOUNTY/TOKEN_BOUNTY).
-
-    FIN.
-
-    INCENTIVES: 
-        - Because 10% of winnings is claimable by ANY shooters dead or alive on the
-        winning faction, it encourages people to make at the very least the min shot.
-        - Killing Remilio's stimulates gameplay because a dead remilio can't shoot and the holder would need to trade/buy a new one if they want to keep playing.
-        - Killing Whale's has the same effect, incentivizes whales to re-enter.
-        - People will want to jump factions, this stimulates trading.
-        - People work together on a public strategy to kill particular factions.
-        - Drives community, creates a lot of engagement, bringing attention to
-        the project.
-        - EMERGENT METAS FROM COMMUNITY, WHO KNOWS WHAT THESE WILL BE.
-
-    NOTES:
-        - The WAR will open with a specific cutoff time and date. This will not
-          be modifiable. PERIOD. IT IS PROGRAMMED.
-        - NO NEW ACTIONS will be permitted after the CUTOFF DATE.
-        - CLAIMS will only be open after the CUTOFF DATE.
-        - DEV will ONLY be able to access 33% stake once CUT OFF DATE IS REACHED.
-        - Any funds in FINAL_BOUNTY will ONLY be accessible to Rem64 claims. Dev
-          can never access these. As such there's a chance some ether is locked away permanently from never be claimed.
-
-
-
-
-    RANDOM IDEAS:
-        - If player attacked and not killed, dmg dealt should be 50%. This disincentivizes
-          a group of small players from attacking a 'whale'.
-        - Should be able to reinforce an ally. This will 'donate' bounty. This is useful for
-          protecting whales, or 'storing faction value'.
-        - 
-    */
 
     // Initialization and constructor
     IRem64 Rem64;
@@ -248,22 +157,15 @@ contract RemWar is Ownable, ReentrancyGuard {
         factionBounty[Rem64.getFaction(tokenId)] -= bounty;
     }
 
-    function subFromBountyNotDead(uint256 tokenId, uint256 bounty) private returns (uint256) {
-        uint256 damage = Math.mulDiv(bounty, bounty, remBounty[tokenId]);
-        remBounty[tokenId] -= damage;
-        factionBounty[Rem64.getFaction(tokenId)] -= damage;
-        return damage;
-    }
-
     // Helper function to add shooter to has shot list
     // and increment faction shooter counter
     mapping(uint256 => bool) public hasShot;
 
-    uint256[] factionShooters = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    uint256[] factionShooterCount = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
     function shottaAdd(uint256 tokenId) private {
         hasShot[tokenId] = true;
-        factionShooters[Rem64.getFaction(tokenId)] += 1;
+        factionShooterCount[Rem64.getFaction(tokenId)] += 1;
     }
 
     // REAL KILLAS 💧🩸 call this function
@@ -278,9 +180,9 @@ contract RemWar is Ownable, ReentrancyGuard {
         checkFriendlyFire(shotta, target)
         minShotPrice(msg.value)
     {
-        require((msg.value % 0.001 ether) == 0, "NOT A MULTIPLE OF MIN SHOT");
         uint256 shotPrice = msg.value;
-
+        require((shotPrice % 0.001 ether) == 0, "NOT A MULTIPLE OF MIN SHOT");
+        
         //Split payment
         FINAL_BOUNTY += Math.mulDiv(shotPrice, 57, 100);
         SHOOTER_BOUNTY += Math.mulDiv(shotPrice, 10, 100);
@@ -311,8 +213,9 @@ contract RemWar is Ownable, ReentrancyGuard {
 
             // He's clapped 👏 but still moving.
             if (remBounty[target] > shotPrice) {
-                uint256 damage = subFromBountyNotDead(target, shotPrice);
+                uint256 damage = Math.mulDiv(shotPrice, shotPrice, remBounty[target]);
                 addToBounty(shotta, shotPrice + damage);
+                subFromBounty(target, damage);
                 shottaAdd(shotta);
                 emit Shot(shotta, target, shotPrice);
                 return;
@@ -470,7 +373,7 @@ contract RemWar is Ownable, ReentrancyGuard {
         require(hasShot[tokenId] == true, "Shooter never shot");
         require(shooterClaimed[tokenId] != true, "Already Claimed");
 
-        uint256 numberOfShooters = factionShooters[Rem64.getFaction(tokenId)];
+        uint256 numberOfShooters = factionShooterCount[Rem64.getFaction(tokenId)];
         uint256 withdrawAmount = Math.mulDiv(SHOOTER_BOUNTY, 1, numberOfShooters);
 
         (bool success, ) = payable(address(msg.sender)).call{
